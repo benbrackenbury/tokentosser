9745df149556812a569dab93b149197db0bef920
Ben Brackenbury <benbrack22@icloud.com>
2026-05-02 15:12:36 +0100

    feat: add full screen gif

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,10 +19,13 @@
             </style>
         @endif
     </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <section class="text-center space-y-4">
-            <h1 class="text-7xl font-bold">Token Tosser</h1>
-            <p class="text-2xl font-medium opacity-60">Coming Soon</p>
+    <body class="">
+        <section class="">
+            <img
+                src="tokentosser.gif"
+                alt="token tosser"
+                class="w-screen h-screen"
+            />
         </section>
     </body>
 </html>
